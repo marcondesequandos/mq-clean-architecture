6f52e5165d5b119b10e53543ebe55652ffb9c89e
felipe.carvalho.glo <felipe.carvalho@globalhitss.com.br>
2023-08-07 18:27:18 -0300

    corrigindo unit test

--- src/usecase/product/create/create.product.unit.spec.ts
+++ src/usecase/product/create/create.product.unit.spec.ts
@@ -3,34 +3,33 @@ import CreateProductUseCase from "./create.product.usecase";
 const input = {
   type: "a",
   name: "Rex",
-  price: 50
-}
+  price: 50,
+};
 
-const MockRepository = ()=> {
+const MockRepository = () => {
   return {
     find: jest.fn(),
     findAll: jest.fn(),
     create: jest.fn(),
-    update: jest.fn()
-  }
-}
+    update: jest.fn(),
+  };
+};
 
-
-describe("Unit test create product use case", ()=> {
-  it("should create a product", async ()=> {
+describe("Unit test create product use case", () => {
+  it("should create a product", async () => {
     const productRepository = MockRepository();
     const productCreateUseCase = new CreateProductUseCase(productRepository);
 
-    const output = await productCreateUseCase.execute(input)
+    const output = await productCreateUseCase.execute(input);
 
     expect(output).toEqual({
       id: expect.any(String),
       name: input.name,
-      price: input.price
+      price: input.price,
     });
   });
 
-  it("should throw an error when name is missing", async ()=> {
+  it("should throw an error when name is missing", async () => {
     const productRepository = MockRepository();
     const productCreateUseCase = new CreateProductUseCase(productRepository);
 
@@ -40,11 +39,11 @@ describe("Unit test create product use case", ()=> {
       "Name is required"
     );
   });
-  it("should throw an error when price is missing", async ()=> {
+  it("should throw an error when price is missing", async () => {
     const productRepository = MockRepository();
     const productCreateUseCase = new CreateProductUseCase(productRepository);
 
-    input.name = "Rex"
+    input.name = "Rex";
 
     input.price = 0;
 
@@ -52,4 +51,15 @@ describe("Unit test create product use case", ()=> {
       "Price must be greater than zero"
     );
   });
-})
+
+  it("should throw an error when name and price are missing", async () => {
+    const productRepository = MockRepository();
+    const productCreateUseCase = new CreateProductUseCase(productRepository);
+    input.name = "";
+    input.price = 0;
+
+    await expect(productCreateUseCase.execute(input)).rejects.toThrow(
+      "product: Name is required,product: Price must be greater than zero"
+    );
+  });
+});
